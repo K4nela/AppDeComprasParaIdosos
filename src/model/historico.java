2ecadc2f42927a6e972d2bec73547be756a9d4a5
Madu030 <eduardanjos03@gmail.com>
2025-10-22 10:21:07 -0300

    UPDATE historico

--- src/model/historico.java
+++ src/model/historico.java
@@ -8,10 +8,16 @@ public class historico {
     private int id_item;
     private int id_status;
 
+    public historico(int id_histrico, Date data_historico, int id_item, int id_status) {
+        this.id_histrico = id_histrico;
+        this.data_historico = data_historico;
+        this.id_item = id_item;
+        this.id_status = id_status;
+    }
+
     public int getId_histrico() {
         return id_histrico;
     }
-
     public void setId_histrico(int id_histrico) {
         this.id_histrico = id_histrico;
     }
@@ -19,7 +25,6 @@ public class historico {
     public Date getData_historico() {
         return data_historico;
     }
-
     public void setData_historico(Date data_historico) {
         this.data_historico = data_historico;
     }
@@ -27,7 +32,6 @@ public class historico {
     public int getId_item() {
         return id_item;
     }
-
     public void setId_item(int id_item) {
         this.id_item = id_item;
     }
@@ -35,15 +39,18 @@ public class historico {
     public int getId_status() {
         return id_status;
     }
-
     public void setId_status(int id_status) {
         this.id_status = id_status;
     }
 
-    public historico(int id_histrico, Date data_historico, int id_item, int id_status) {
-        this.id_histrico = id_histrico;
-        this.data_historico = data_historico;
-        this.id_item = id_item;
-        this.id_status = id_status;
+    @Override
+    public String toString() {
+        return "\n------- Histórico -------" +
+                "\nID do histórico: " + id_histrico +
+                "\nData do histórico: " + data_historico +
+                "\nID do item: " + id_item +
+                "\nID do status: " + id_status +
+                "\n-------------------------";
     }
+
 }

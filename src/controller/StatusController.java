e4bebc6f5ad31fd65673fbcd32711fbb23acfe44
Madu030 <eduardanjos03@gmail.com>
2025-11-01 07:05:36 -0300

    UPDATE StatusController

--- src/controller/StatusController.java
+++ src/controller/StatusController.java
@@ -9,27 +9,15 @@ import java.util.List;
 public class StatusController {
     private StatusDAO statusDao;
 
-    public StatusController(Connection conn) {
-        this.statusDao = new StatusDAO(conn);
-    }
+    public StatusController(Connection conn) { this.statusDao = new StatusDAO(conn);}
 
-    public void criarStatus(status s) throws SQLException {
-        statusDao.insert(s);
-    }
+    public void criarStatus(status s) throws SQLException { statusDao.insert(s);}
 
-    public List<status> listarTodos() throws SQLException {
-        return statusDao.getAll();
-    }
+    public List<status> listarTodos() throws SQLException { return statusDao.getAll();}
 
-    public status buscarPorId(int id) throws SQLException {
-        return statusDao.getById(id);
-    }
+    public status buscarPorId(int id) throws SQLException { return statusDao.getById(id);}
 
-    public void atualizarStatus(status s) throws SQLException {
-        statusDao.update(s);
-    }
+    public void atualizarStatus(status s) throws SQLException { statusDao.update(s);}
 
-    public void deletarStatus(int id) throws SQLException {
-        statusDao.delete(id);
-    }
+    public void deletarStatus(int id) throws SQLException { statusDao.delete(id);}
 }
